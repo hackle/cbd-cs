f482bf16002042c00de14f504a15f8262a874b1f
Hacke Wayne <hacklew@hotmail.com>
2020-11-05 07:25:01 +1300

    rename inflation to interest

---
+++ 7.hard-saver.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+public static class HardSaver
+{
+    /*
+        Calculate total income over the years in regards to interest
+        Presumed interest rate is constant
+        See spec for example
+        Tip: you'll need the Aggregate function
+    */
+    public static decimal AllWithInterest(IEnumerable<decimal> annualIncome, decimal interestRate) 
+    {
+        return 0;
+    }
+}
--- 7.hard-saver.tests.cs
+++ 7.hard-saver.tests.cs
@@ -1,21 +1,21 @@
 using NUnit.Framework;
 
-public class InflatedIncomeTests 
+public class HardSaverTests 
 {
     [Test]
     public void Snowballs() 
     {
         var annualIncome = new decimal[]
         {
-            50000, // current year, no inflation
+            50000, // current year, no interest
             40000, // a year before
             30000, // 2 years before
         };
 
-        var actual = InflatedIncome.Calculate(annualIncome, 0.02m);
+        var actual = HardSaver.AllWithInterest(annualIncome, 0.02m);
 
         // disclaimer: not to be taken seriously
-        // current year, no inflation yet
+        // current year, no interest yet
         // 50000 + 40000 * 1.02 + 30000 * 1.02 * 1.02
         Assert.That(actual, Is.EqualTo(122012));
     }
